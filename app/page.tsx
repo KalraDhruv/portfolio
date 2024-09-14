ba1f5543857fde55d601ce56b2d034efa5e9efd7
Dhruv <dhruvkalra@proton.me>
2024-09-14 19:57:31 -0400

    Borders and buttons remaining

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,25 @@ import "./globals.css";
 export default function Home() {
 	return (
 		<main>
-			<div className="relative w-full h-screen flex place-items-center">
-				<div className="relative flex-col items-center inline-flex justify-center lg:flex-row w-full px-4 lg:px-10">
-					<h1 className=" lg:absolute xl:text-5xl lg:text-4xl w-full pl-40 sm:text-5xl font-bold  mt-36 leading-tight z-10">
-						THE OLD AND WISE<br /> MAJESTIC OWL <br />READY TO DIVE INTO<br /> PROGRAMMING
+			<div className="relative w-full h-full">
+				<div className="relative flex-col items-center flex justify-center px-4 lg:px-10">
+					<h1 className=" lg:absolute relative tracking-widest lg:text-5xl lg:w-full lg:pl-10 sm:text-5xl font-semibold font-code my-10 lg:mt-0 leading-tight z-10">
+						The old and wise<br /> majestic owl<br />ready to dive into<br /> programming 
 					</h1>
-
-					<div className="relative w-7/12 aspect-video ">
+					<h2 className="hidden lg:block absolute right-0 top-2 tracking-widest lg:text-5xl lg:pr-10 font-semibold font-code leading-tight z-10">
+						Curious and<br/> ready to start <br/> a new adventure 
+					</h2>
+					<h2 className="hidden xl:block absolute right-0 bottom-12 tracking-widest lg:text-5xl lg:pr-10 font-semibold font-code leading-tight z-10">
+						Interested in <br/> working on <br/> thought provoking<br/>projects
+					</h2>
+					<div className="relative lg:relative lg:mt-10 xl:mt-14 sm:w-1/2 lg:w-7/12 aspect-video">
 						<Image
 							src="/images/owl.jpg"
 							layout="fill"
 							objectFit="cover"
 							alt="My Picture"
-							className="bottom-0 right-0 rounded-3xl backdrop-blur-sm" />
+							className="rounded-3xl backdrop-blur-sm" />
+
 					</div>
 
 				</div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,8 @@ module.exports= {
 	screens: {
 		sm: '640px',
 		md: '768px',
-		lg: '1024px',
-		xl: '1280px',
-		'2xl': '1536px',
+		lg: '1280px',
+		xl: '1536px',
 	},
   	extend:{
   	        fontFamily:{
