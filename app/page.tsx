973e9aa61fd0948f9f0ad4b2983d5c5ff588ccad
Dhruv <dhruvkalra@proton.me>
2024-09-14 11:59:21 -0400

    Mobile Interactive Added Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,26 @@
 import Image from "next/image";
 import "./globals.css";
 export default function Home() {
-  return (
-	 <main> 
-	 <div className="ml-4 mt-8 relative aspect-video w-1/2 ">
-			<Image
-			src="/images/owl.jpg"
-			layout="fill"
-			objectFit="cover"
-			alt="My Picture"
-			className="absolute rounded-3xl backdrop-blur-sm bottom-4 right-4"/>
-	 </div>
-	 </main>
-	  
-	  );
+	return (
+		<main>
+			<div className="relative w-full h-screen flex ">
+				<div className="relative flex-col items-center inline-flex justify-center lg:flex-row w-full px-4 lg:px-10">
+					<h1 className="lg:absolute xl:text-7xl sm:text-6xl font-bold mb-6 leading-tight z-10">
+						THE OLD AND WISE<br /> MAJESTIC OWL <br />READY TO DIVE INTO<br /> PROGRAMMING
+					</h1>
+
+					<div className="relative lg:ml-36 mt-12 bottom-0 right-0 aspect-video w-7/12 ">
+						<Image
+							src="/images/owl.jpg"
+							layout="fill"
+							objectFit="cover"
+							alt="My Picture"
+							className="rounded-3xl backdrop-blur-sm" />
+					</div>
+
+				</div>
+			</div>
+		</main>
+
+	);
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,27 +4,24 @@ import Navbar from '@/components/Navbar'
 
 function Header(): JSX.Element {
 	return(
-		<header className="py-8 xl:py-12 bg-darkCharcoal text-lightGrey font-code font-semibold">
+		<header className="py-4 xl:py-8  bg-darkCharcoal text-lightGrey font-code font-semibold">
 			<div className="container mx-auto flex justify-between items-center">
 				<Link href='/'>
-				<h2 className=" text-4xl hover:text-modernCyan">
-					Dhruv
+				<h2 className="text-2xl 2xl:text-4xl xl:text-3xl hover:text-modernCyan">
+					The Wise Owl	
 					<span className="text-modernCyan">
 						.
 					</span>
 				</h2>
 				</Link>
 				 {/** Navbar only for desktops and large screens*/}
-				<aside className="hidden xl:flex justify-end items-center gap-8 ">
+				<aside className="hidden lg:flex justify-end items-center gap-8 ">
 					<Navbar />
 				</aside>
 				{/** Navbar for mobiles and smaller screens */}
-				<aside className="xl:hidden">
+				<aside className="lg:hidden">
 					<p>mobile nav</p>
 				</aside>
-
-
-
 			</div>
 		</header>
 	);
